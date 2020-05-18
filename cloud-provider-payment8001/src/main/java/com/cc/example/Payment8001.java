453bc22cd2efbbf4a59b013b6e18f6ee546a1437
cao.chen <cao.chen@yitu-inc.com>
2020-05-18 22:09:38 +0800

    初始化

---
+++ cloud-provider-payment8001/src/main/java/com/cc/example/Payment8001.java
@@ -0,0 +1,14 @@
+package com.cc.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class Payment8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(Payment8001.class, args);
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/cc/example/web/PaymentController.java
@@ -0,0 +1,15 @@
+package com.cc.example.web;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author cc
+ * @since 2020/05/18 22:02
+ **/
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+}
